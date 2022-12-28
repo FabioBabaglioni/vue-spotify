e0574f2424fbed218a7d098e1bd2628757248a41
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-12-28 11:21:27 +0100

    Sacafolding iniziale

---
+++ src/components/AppMain.vue
@@ -0,0 +1,30 @@
+<script >
+import MainSidebar from "./MainSidebar.vue"
+import MainHeader from "./MainHeader.vue"
+import MainContent from "./MainContent.vue"
+
+
+
+export default {
+    components: {
+        MainSidebar, MainHeader, MainContent
+    }
+}
+
+</script>
+
+<template>
+    <main>
+        <MainSidebar />
+        <MainContent />
+        <MainHeader />
+    </main>
+
+
+</template>
+
+<style scoped>
+main {
+    height: 100%;
+}
+</style>
---
+++ src/components/MainSidebar.vue
@@ -0,0 +1,23 @@
+<script >
+
+export default {
+
+}
+
+</script>
+
+<template>
+    <section id="sidebar">
+
+    </section>
+</template>
+
+<style scoped lang="scss">
+@use "../style/partials/variables.scss" as *;
+
+#sidebar {
+    width: 200px;
+    height: calc(100vh - 100px);
+    background-color: $brandThird;
+}
+</style>
