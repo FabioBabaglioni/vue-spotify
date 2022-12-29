1388fc4130d9c8dfa5696f9928acccb40755284a
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-12-29 10:48:31 +0100

    Refactoring

--- src/components/Mainheader.vue
+++ src/components/Mainheader.vue
@@ -15,7 +15,7 @@ export default {
                             class="icon" /></span></a>
                 <a href=""><span><font-awesome-icon icon="fa-solid fa-circle-arrow-left" class="icon" /></span></a>
             </div>
-            <button>
+            <button class="pe-3 ps-1">
                 <span id="user"><font-awesome-icon icon="fa-regular fa-user" /></span>
                 <span class="px-3">fabiobabaglioni</span>
                 <font-awesome-icon icon="fa-solid fa-caret-down" />
@@ -46,10 +46,15 @@ button {
     color: $brandPrimary;
     background-color: #111a19;
 
+
     #user {
         padding: 5px;
         background-color: #535353;
         border-radius: 50%;
+    }
+
+    &:hover {
+        background-color: #282828;
     }
 }
 </style> 
