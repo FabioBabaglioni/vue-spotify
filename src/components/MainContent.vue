019e0049d441cf3de01ac4496e64bc2d31cccd1a
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-12-29 10:48:15 +0100

    Aggiunta card ascoltati di recente

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -8,7 +8,20 @@ export default {
 
 <template>
     <section id="content">
+        <div class="container_site">
+            <h2 class="pb-5">Buongiorno</h2>
 
+            <div class="d-flex flex-wrap justify-content-between">
+                <div class="flex-row align-items-center card">
+                    <img class="img_ultimi pe-3" src="../img/cure.jpeg" alt="">
+                    <div class="title">Titolo della canzone</div>
+                    <div class="ps-5">
+                        <font-awesome-icon icon="fa-solid fa-circle-play" class="play" />
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </section>
 </template>
 
@@ -17,7 +30,44 @@ export default {
 
 #content {
     width: calc(100vw - 220px);
-    height: calc(100vh - 200px);
+    height: calc(100vh - 100px);
     background-color: $brandSfondo;
+    padding-top: 120px;
+
+    .card {
+        width: calc((100% / 3) - 10px);
+        background-color: hsla(0, 0%, 100%, .1);
+
+        .img_ultimi {
+            width: 25%;
+        }
+
+        .title {
+            font-size: 18px;
+            color: $brandPrimary;
+        }
+
+        .play {
+            color: $brandLogo;
+            font-size: 40px;
+            padding-left: 30px;
+            display: none;
+
+            &:hover {
+                font-size: 45px;
+            }
+        }
+
+        &:hover {
+            background-color: hsla(0, 0%, 100%, .2);
+            cursor: pointer;
+
+            .play {
+                display: inline;
+            }
+        }
+    }
+
+
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,9 +4,10 @@ import App from './App.vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faHouse, faMagnifyingGlass, faBook, faSquarePlus, faHeart, faCircleArrowLeft, faCircleArrowRight, faUser as fasUser, faCaretDown } from '@fortawesome/free-solid-svg-icons'
+import { faHouse, faMagnifyingGlass, faBook, faSquarePlus, faHeart, faCircleArrowLeft, faCircleArrowRight, faUser as fasUser, faCaretDown, faCirclePlay } from '@fortawesome/free-solid-svg-icons'
 import { faUser as farUser } from '@fortawesome/free-regular-svg-icons'
 
-library.add(faHouse, faMagnifyingGlass, faBook, faSquarePlus, faHeart, faCircleArrowLeft, faCircleArrowRight, fasUser, farUser, faCaretDown)
+
+library.add(faHouse, faMagnifyingGlass, faBook, faSquarePlus, faHeart, faCircleArrowLeft, faCircleArrowRight, fasUser, farUser, faCaretDown, faCirclePlay)
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
