8b6a55238db36c499816814294f5a3e7cb850dae
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-12-29 10:51:56 +0100

    Refactoring componente recenti

---
+++ src/components/ContentCardRecenti.vue
@@ -0,0 +1,61 @@
+<script >
+
+export default {
+
+}
+
+</script>
+
+<template>
+
+    <h2 class="pb-5">Buongiorno</h2>
+
+    <div class="d-flex flex-wrap justify-content-between">
+        <div class="flex-row align-items-center card">
+            <img class="img_ultimi pe-3" src="../img/cure.jpeg" alt="">
+            <div class="title">Titolo della canzone</div>
+            <div class="ps-5">
+                <font-awesome-icon icon="fa-solid fa-circle-play" class="play" />
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use "../style/partials/variables.scss" as *;
+
+.card {
+    width: calc((100% / 3) - 10px);
+    background-color: hsla(0, 0%, 100%, .1);
+
+    .img_ultimi {
+        width: 25%;
+    }
+
+    .title {
+        font-size: 18px;
+        color: $brandPrimary;
+    }
+
+    .play {
+        color: $brandLogo;
+        font-size: 40px;
+        padding-left: 30px;
+        display: none;
+
+        &:hover {
+            font-size: 45px;
+        }
+    }
+
+    &:hover {
+        background-color: hsla(0, 0%, 100%, .2);
+        cursor: pointer;
+
+        .play {
+            display: inline;
+        }
+    }
+}
+</style>
--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,7 +1,9 @@
 <script>
-
+import ContentCardRecenti from "./ContentCardRecenti.vue"
 export default {
-
+    components: {
+        ContentCardRecenti,
+    }
 }
 
 </script>
@@ -9,18 +11,7 @@ export default {
 <template>
     <section id="content">
         <div class="container_site">
-            <h2 class="pb-5">Buongiorno</h2>
-
-            <div class="d-flex flex-wrap justify-content-between">
-                <div class="flex-row align-items-center card">
-                    <img class="img_ultimi pe-3" src="../img/cure.jpeg" alt="">
-                    <div class="title">Titolo della canzone</div>
-                    <div class="ps-5">
-                        <font-awesome-icon icon="fa-solid fa-circle-play" class="play" />
-                    </div>
-                </div>
-
-            </div>
+            <ContentCardRecenti />
         </div>
     </section>
 </template>
@@ -33,41 +24,5 @@ export default {
     height: calc(100vh - 100px);
     background-color: $brandSfondo;
     padding-top: 120px;
-
-    .card {
-        width: calc((100% / 3) - 10px);
-        background-color: hsla(0, 0%, 100%, .1);
-
-        .img_ultimi {
-            width: 25%;
-        }
-
-        .title {
-            font-size: 18px;
-            color: $brandPrimary;
-        }
-
-        .play {
-            color: $brandLogo;
-            font-size: 40px;
-            padding-left: 30px;
-            display: none;
-
-            &:hover {
-                font-size: 45px;
-            }
-        }
-
-        &:hover {
-            background-color: hsla(0, 0%, 100%, .2);
-            cursor: pointer;
-
-            .play {
-                display: inline;
-            }
-        }
-    }
-
-
 }
 </style>
