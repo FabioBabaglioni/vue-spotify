3c309971b8a0fd483d5fb9c9362a39c5b86b6949
FabioBabaglioni <fabiobabaglioni@gmail.com>
2023-01-04 11:38:28 +0100

    icone

--- src/main.js
+++ src/main.js
@@ -4,10 +4,11 @@ import App from './App.vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faHouse, faMagnifyingGlass, faBook, faSquarePlus, faHeart, faCircleArrowLeft, faCircleArrowRight, faUser as fasUser, faCaretDown, faCirclePlay } from '@fortawesome/free-solid-svg-icons'
-import { faUser as farUser } from '@fortawesome/free-regular-svg-icons'
+import { faHouse, faMagnifyingGlass, faBook, faSquarePlus, faHeart, faCircleArrowLeft, faCircleArrowRight, faUser as fasUser, faCaretDown, faCirclePlay, faShuffle, faBackwardStep, faForwardStep, faRepeat } from '@fortawesome/free-solid-svg-icons'
+import { faUser as farUser, faHeart as fasHeart } from '@fortawesome/free-regular-svg-icons'
 
 
-library.add(faHouse, faMagnifyingGlass, faBook, faSquarePlus, faHeart, faCircleArrowLeft, faCircleArrowRight, fasUser, farUser, faCaretDown, faCirclePlay)
+library.add(faHouse, faMagnifyingGlass, faBook, faSquarePlus, faHeart, faCircleArrowLeft, faCircleArrowRight, fasUser, farUser, faCaretDown, faCirclePlay, fasHeart, faShuffle, faBackwardStep, faForwardStep, faRepeat)
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+
